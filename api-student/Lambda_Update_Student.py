98147c37fce6007bb28111a7fe3873e34d61723b
Jorge Eduardo Melgarejo Castillo <jorge.melgarejo@utec.edu.pe>
2024-11-13 13:31:40 -0500

    si

--- api-student/Lambda_Update_Student.py
+++ api-student/Lambda_Update_Student.py
@@ -1,30 +1,45 @@
 import boto3
 import json
+import os
 
 def lambda_handler(event, context):
     # Obtener token de autorización
-    token = event['headers']['Authorization']
+    token = event['headers'].get('Authorization')
+    if not token:
+        return {
+            'statusCode': 400,
+            'body': 'Falta el token de autorización'
+        }
+
+    # Obtener el nombre de la función de validación desde las variables de entorno
+    validate_function_name = os.environ.get("VALIDATE_FUNCTION_NAME", "DefaultValidateFunctionName")
 
     # Validar el token y extraer el student_id
     lambda_client = boto3.client('lambda')
-    payload_string = json.dumps({ "token": token })
+    payload_string = json.dumps({"token": token})
     invoke_response = lambda_client.invoke(
-        FunctionName="ValidarTokenAcceso",
+        FunctionName=validate_function_name,
         InvocationType='RequestResponse',
         Payload=payload_string
     )
     response = json.loads(invoke_response['Payload'].read())
 
     # Verificar si el token es válido
-    if response['statusCode'] == 403:
+    if response.get('statusCode') == 403:
         return {
             'statusCode': 403,
             'body': 'Forbidden - Acceso No Autorizado'
         }
 
     # Extraer el student_id y tenant_id desde la respuesta de validación
-    student_id = response['student_id']
-    tenant_id = response['tenant_id']
+    student_id = response.get('student_id')
+    tenant_id = response.get('tenant_id')
+
+    if not student_id or not tenant_id:
+        return {
+            'statusCode': 500,
+            'body': 'Error: Falta student_id o tenant_id en la respuesta de ValidateAccessToken'
+        }
 
     # Obtener los datos de actualización desde el evento
     update_data = json.loads(event['body'])
