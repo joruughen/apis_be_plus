d7307ec18181638270c1395d2864a24ec919403f
Jorge Eduardo Melgarejo Castillo <jorge.melgarejo@utec.edu.pe>
2024-11-29 12:48:06 -0500

    nodejs  :D

--- apis-nodejs/api-activities/api-activities/Lambda_CreateActivity.js
+++ apis-nodejs/api-activities/api-activities/Lambda_CreateActivity.js
@@ -2,6 +2,7 @@ const { LambdaClient, InvokeCommand } = require('@aws-sdk/client-lambda');
 const { DynamoDBClient } = require('@aws-sdk/client-dynamodb');
 const { DynamoDBDocumentClient, GetCommand, PutCommand } = require('@aws-sdk/lib-dynamodb');
 const moment = require('moment');
+const { v4: uuidv4 } = require('uuid'); // Importa la función v4 de uuid para generar UUIDs
 
 // Crear clientes para DynamoDB y Lambda usando la versión modular
 const lambdaClient = new LambdaClient({});
@@ -13,20 +14,6 @@ const TOKENS_TABLE = `${process.env.STAGE}_t_access_tokens`;
 
 exports.handler = async (event, context) => {
   try {
-    // Imprime el cuerpo recibido antes de procesarlo para depuración
-    console.log("Received event body:", event.body);
-
-    // Accedemos directamente a las propiedades de event.body como objeto
-    const { activity_id, activitie_type, time } = event.body;
-
-    // Validar los parámetros requeridos
-    if (!activity_id || !activitie_type) {
-      return {
-        statusCode: 400,
-        body: JSON.stringify({ error: 'Missing required parameters: activity_id or activitie_type' })
-      };
-    }
-
     // Obtener el token de autorización desde los headers
     const token = event.headers['Authorization'];
     if (!token) {
@@ -82,6 +69,20 @@ exports.handler = async (event, context) => {
       };
     }
 
+    // Validar los datos del body
+    const body = event.body;
+    const { activitie_type, time } = body; // activity_id ahora será generado automáticamente
+
+    if (!activitie_type) {
+      return {
+        statusCode: 400,
+        body: JSON.stringify({ error: 'Missing activity_type in request body' })
+      };
+    }
+
+    // Generar un activity_id único utilizando UUID
+    const activity_id = uuidv4();  // Generación de UUID para el activity_id
+
     // Crear objeto de la actividad
     const creationDate = moment().format('YYYY-MM-DD HH:mm:ss');
 
@@ -101,7 +102,7 @@ exports.handler = async (event, context) => {
     // Verificar si la actividad ya existe para este student_id y tenant_id
     const existingActivity = await docClient.send(new GetCommand({
       TableName: ACTIVITIES_TABLE,
-      Key: { tenant_id: tenantId, activity_id: activity_id }
+      Key: { tenant_id: tenantId, activity_id: activity_id, student_id: studentId }
     }));
 
     if (existingActivity.Item) {
